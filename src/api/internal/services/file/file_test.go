7480488780ad7c0336781b0ba8a58bbd1362f044
Michael Kao <github@47north.lat>
2025-03-07 23:08:59 -0800

    test: remove unused k8s/client-go/kubernetes/fake import

--- src/api/internal/services/file/file_test.go
+++ src/api/internal/services/file/file_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/rest"
 	k8s "k8s.io/client-go/kubernetes"
 
