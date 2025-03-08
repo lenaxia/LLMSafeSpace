c8b95992468d91960f4cb87e387dc90ac7e59abf
Mike Kao <mikekao@amazon.com>
2025-03-08 10:19:36 -0800

    refactor: Remove unused kubernetes package import

--- src/api/internal/services/services.go
+++ src/api/internal/services/services.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/lenaxia/llmsafespace/api/internal/config"
 	"github.com/lenaxia/llmsafespace/api/internal/interfaces"
-	"github.com/lenaxia/llmsafespace/api/internal/kubernetes"
 	"github.com/lenaxia/llmsafespace/api/internal/logger"
 	"github.com/lenaxia/llmsafespace/api/internal/services/auth"
 	"github.com/lenaxia/llmsafespace/api/internal/services/cache"
