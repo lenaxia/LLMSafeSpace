b0ccda2acb578d624b11d960c70643b135c0b634
Michael Kao <github@47north.lat>
2025-03-12 13:31:27 -0700

    chore: Use installed deepcopy-gen binary from GOPATH/bin

--- hack/update-deepcopy.sh
+++ hack/update-deepcopy.sh
@@ -8,7 +8,7 @@ SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo "$(go env GOPATH)/pkg/mod/k8s.io/code-generator@v0.26.0")}
 
 # Generate deepcopy functions
-"${CODEGEN_PKG}/cmd/deepcopy-gen" \
+"$(go env GOPATH)/bin/deepcopy-gen" \
   --input-dirs "github.com/lenaxia/llmsafespace/api/internal/types" \
   --output-package "github.com/lenaxia/llmsafespace/api/internal/types" \
   --output-file-base zz_generated.deepcopy \
