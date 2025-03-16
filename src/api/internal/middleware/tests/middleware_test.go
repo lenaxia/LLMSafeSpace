4344d699a398382f7f41c5cb2f1556ab064743cc
Michael Kao <github@47north.lat>
2025-03-15 23:36:26 -0700

    chore: Remove unused imports in middleware_test.go

--- src/api/internal/middleware/tests/middleware_test.go
+++ src/api/internal/middleware/tests/middleware_test.go
@@ -1,26 +1,10 @@
 package tests
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"math/rand"
-	"net"
 	"os"
-	"regexp"
-	"strings"
-	"sync"
 	"testing"
-	"time"
-	"unicode"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/trace"
 )
 
 func TestMain(m *testing.M) {
