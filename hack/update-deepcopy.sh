101d2df3bd7bc64f5fd63ce71a9c7969ab6751bf
Michael Kao <github@47north.lat>
2025-03-18 23:20:18 -0700

    blah
    Signed-off-by: Mike Kao <mikekao@amazon.com>

--- hack/update-deepcopy.sh
+++ hack/update-deepcopy.sh
@@ -1,23 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/../src/api
-MODULE_PATH="github.com/lenaxia/llmsafespace/api"
-TYPES_PKG="${MODULE_PATH}/internal/types"
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
-# Generate deepcopy functions with verbose logging
-"$(go env GOPATH)/bin/deepcopy-gen" \
-  --v 5 \
-  --input-dirs "${TYPES_PKG}" \
-  --output-package "${TYPES_PKG}" \
-  --output-file-base zz_generated.deepcopy \
-  --bounding-dirs "${TYPES_PKG}" \
-  --go-header-file "${SCRIPT_ROOT}/../hack/boilerplate.go.txt"
+# Source the helper functions
+source "${SCRIPT_ROOT}/hack/kube_codegen.sh"
 
-# Format the generated code
-go fmt "./src/api/internal/types/zz_generated.deepcopy.go"
+# Call the helper function
+# The argument should be the root directory where your Go files are located
+kube::codegen::gen_helpers "${SCRIPT_ROOT}"
 
-echo "DeepCopy generation completed successfully"
