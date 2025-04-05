d31556add54109f468ee4d8ebacf31b19e0960a7
Lenaxia <github@47north.lat>
2025-04-04 21:34:26 -0700

    start over
    Signed-off-by: Lenaxia <github@47north.lat>

--- pkg/kubernetes/tests/run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Run all tests in the kubernetes package
-cd "$(dirname "$0")"
-go test -v ./...
--- pkg/types/doc.go
+++
@@ -1,4 +0,0 @@
-// +k8s:deepcopy-gen=package
-
-// Package types contains API Schema definitions for the LLMSafeSpace API
-package types
