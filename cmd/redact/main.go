353c9a5a196a8ff38d2856677545b564266281e3
Lenaxia <github@47north.lat>
2026-05-22 07:06:38 +0000

    US-1.5: Build redact binary with 16 patterns, NewRedactorFromFile, sync.Once caching

---
+++ cmd/redact/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/lenaxia/llmsafespace/pkg/redact"
+)
+
+func main() {
+	configPath := flag.String("config", "/sandbox-cfg/redact-patterns.json", "path to extra patterns JSON file")
+	flag.Parse()
+
+	r, err := redact.NewRedactorFromFile(*configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "redact: %v\n", err)
+		os.Exit(1)
+	}
+
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "redact: failed to read stdin: %v\n", err)
+		os.Exit(1)
+	}
+
+	result, err := r.Redact(string(input))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "redact: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprint(os.Stdout, result)
+}
