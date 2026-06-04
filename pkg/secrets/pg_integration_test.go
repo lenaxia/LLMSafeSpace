72bcad626d34da7560a16262e48c1a2c512fb5e6
Mike Kao <mikekao@amazon.com>
2026-06-04 00:48:18 +0000

    fix: gofmt pg_integration_test.go

--- pkg/secrets/pg_integration_test.go
+++ pkg/secrets/pg_integration_test.go
@@ -289,7 +289,7 @@ func TestPgSecretStore_Bindings(t *testing.T) {
 	store.CreateSecret(ctx, s1)
 	store.CreateSecret(ctx, s2)
 
-	wsID := fmt.Sprintf("00000000-0000-4000-8000-%012d", time.Now().UnixNano() % 1000000000000)
+	wsID := fmt.Sprintf("00000000-0000-4000-8000-%012d", time.Now().UnixNano()%1000000000000)
 	ensureTestWorkspace(t, pool, wsID, userID)
 
 	// Set bindings
@@ -403,7 +403,7 @@ func TestPgE2E_FullSecretLifecycle(t *testing.T) {
 	}
 
 	// Bind
-	wsID := fmt.Sprintf("00000000-0000-4000-8001-%012d", time.Now().UnixNano() % 1000000000000)
+	wsID := fmt.Sprintf("00000000-0000-4000-8001-%012d", time.Now().UnixNano()%1000000000000)
 	ensureTestWorkspace(t, pool, wsID, userID)
 	_, err = svc.SetBindings(ctx, userID, wsID, []string{created.ID})
 	if err != nil {
@@ -603,7 +603,7 @@ func TestPgE2E_AddBindings_IdempotentAndConcurrent(t *testing.T) {
 		ids = append(ids, s.ID)
 	}
 
-	wsID := fmt.Sprintf("00000000-0000-4000-8002-%012d", time.Now().UnixNano() % 1000000000000)
+	wsID := fmt.Sprintf("00000000-0000-4000-8002-%012d", time.Now().UnixNano()%1000000000000)
 	ensureTestWorkspace(t, pool, wsID, userID)
 
 	// Idempotent: calling AddBindings 5 times with the same set
@@ -625,7 +625,7 @@ func TestPgE2E_AddBindings_IdempotentAndConcurrent(t *testing.T) {
 	// AddBindings is "merge". The advisory lock serializes them so
 	// the final state is well-defined: whichever ran last wins
 	// the union.
-	wsID2 := fmt.Sprintf("00000000-0000-4000-8003-%012d", time.Now().UnixNano() % 1000000000000)
+	wsID2 := fmt.Sprintf("00000000-0000-4000-8003-%012d", time.Now().UnixNano()%1000000000000)
 	ensureTestWorkspace(t, pool, wsID2, userID)
 	if err := secretStore.SetBindings(ctx, wsID2, []string{ids[0], ids[1]}); err != nil {
 		t.Fatalf("SetBindings: %v", err)
